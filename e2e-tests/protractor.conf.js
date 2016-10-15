e17add1fc95aef7c93784b4d8cbf081faca9ab53
vvvickey8 <vvvickey8@gmail.com>
2016-10-15 15:47:20 +0530

    Update protractor.conf.js

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -11,8 +11,9 @@ exports.config = {
   allScriptsTimeout: 11000,
 
 //seleniumAddress: 'http://localhost:4444/wd/hub'
-seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar'
-  specs: [ '/e2e-tests/movielist_spec.js'
+seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
+  specs: [ 
+	'/e2e-tests/movielist_spec.js'
   ],
 
   capabilities: {
